3127a4094aa1d34534a06e240e8038ccfed6b13d
WeiChiaChang <ioh04019@gmail.com>
2017-11-13 10:10:19 +0800

    ✨ Feat: version 0.1.54 release

--- example/ucantseeme.js
+++ example/ucantseeme.js
@@ -1,4 +1,4 @@
-var john_cena_data = 'https://weichiachang.github.io/easter-eggs-mobile/images/john-cena.png';
+  var john_cena_data = 'https://weichiachang.github.io/easter-eggs-mobile/images/john-cena.png';
   var john_cena_gif = 'https://weichiachang.github.io/easter-eggs-mobile/johncena.gif';
   var john_cena_bg = document.createElement('div');
 
